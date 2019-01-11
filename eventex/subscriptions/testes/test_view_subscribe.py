65bdabcdf14130aea24cbf285a3c11df699c46e7
Gabriel Dantas <marcusgabriel.ds@gmail.com>
2019-01-10 21:28:45 -0300

    Thank you page

--- eventex/subscriptions/testes/test_view_subscribe.py
+++ eventex/subscriptions/testes/test_view_subscribe.py
@@ -48,8 +48,8 @@ class SubscribePostValid(TestCase):
         self.resp = self.client.post('/inscricao/', data)
 
     def test_post(self):
-        """Valid POST shold redirect to /inscricao/"""
-        self.assertEqual(302, self.resp.status_code)
+        """Valid POST shold redirect to /inscricao/1/"""
+        self.assertRedirects(self.resp, '/inscricao/1/')
     
     def test_send_subscribe_email(self):
         self.assertEqual(1, len(mail.outbox))
@@ -82,12 +82,8 @@ class SubscribePostInvalid(TestCase):
         self.assertFalse(Subscription.objects.exists())
 
 
-class SubscribeSuccessMessage(TestCase):
+class SubscriptionDetailNotFound(TestCase):
 
-    def test_message(self):
-        data = dict(name='Gabriel Dantas', cpf='12345678901',
-                    email='marcusgabriel.ds@gmail.com', phone='81-98965-2002')
-        
-        response = self.client.post('/inscricao/', data, follow=True)
-        self.assertContains(response, 'Inscrição realizada com sucesso!')
-
+    def test_not_found(self):
+        resp = self.client.get('/inscricao/0/')
+        self.assertEqual(404, resp.status_code)
