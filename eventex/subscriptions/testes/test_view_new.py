5547c5ed5926bfafe94423b4bd1a934841edbe3f
Gabriel Dantas <marcusgabriel.ds@gmail.com>
2019-01-11 23:36:39 -0300

    Email or phone optional

--- eventex/subscriptions/testes/test_view_new.py
+++ eventex/subscriptions/testes/test_view_new.py
@@ -82,3 +82,11 @@ class SubscriptionNewPostInvalid(TestCase):
     def test_dont_save_subscription(self):
         self.assertFalse(Subscription.objects.exists())
 
+
+class TemplateRegressiontesst(TestCase):
+
+    def test_template_has_non_field_errors(self):
+        invalid_data = dict(name='Gabriel Dantas', cpf='12345678901')
+        response = self.client.post(r('subscriptions:new'), invalid_data)
+
+        self.assertContains(response, '<ul class="errorlist nonfield">')
